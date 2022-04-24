78d88ab377286812a31cfb33bf97aca14fe2073a
anurag <anuragn.0505@gmail.com>
2022-04-24 17:51:31 -0400

    author and articles

--- HospitalProject/Controllers/AuthorController.cs
+++ HospitalProject/Controllers/AuthorController.cs
@@ -1,203 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 using System.Net.Http;
-using System.Diagnostics;
+using System.Web.Mvc;
 using HospitalProject.Models;
-using HospitalProject.Models.ViewModels;
-using System.Web.Script.Serialization;
+using System.Linq;
 
 namespace HospitalProject.Controllers
 {
-    public class ArticleController : Controller
+    public class AuthorController : Controller
     {
         private static readonly HttpClient client;
-        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-        static ArticleController()
+
+        static AuthorController()
         {
-            HttpClientHandler handler = new HttpClientHandler()
-            {
-                AllowAutoRedirect = false,
-                UseCookies = false
-            };
             client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44338/api/");
+            client.BaseAddress = new Uri("https://localhost:44338/");
         }
 
-        /// <summary>
-        /// Gets the authentication cookie sent to this controller.
-        /// </summary>
-        private void GetApplicationCookie()
+        public ActionResult Index(string search)
         {
-            string token = "";
-            client.DefaultRequestHeaders.Remove("Cookie");
+            //string url = "api/author";
+            //HttpResponseMessage response = client.GetAsync(url).Result;
 
-            HttpCookie cookie = System.Web.HttpContext.Current.Request.Cookies.Get(".AspNet.ApplicationCookie");
-            if (cookie != null) token = cookie.Value;
+            //IEnumerable<Author> Authors = response.Content.ReadAsAsync<IEnumerable<Author>>().Result;
 
-            if (token != "") client.DefaultRequestHeaders.Add("Cookie", ".AspNet.ApplicationCookie=" + token);
+            List<Author> authors = db.Authors.ToList();
 
-            return;
+            return View(authors);
+
+
         }
 
-        // GET: Article/List
-        public ActionResult List()
-        {
-            //Objective: retrieve a list of articles
-            //curl https://localhost:44338/api/articledata/listarticles
-
-            string url = "articledata/listarticles";
-            HttpResponseMessage response = client.GetAsync(url).Result;
-
-
-            IEnumerable<ArticlesDto> Articles = response.Content.ReadAsAsync<IEnumerable<ArticlesDto>>().Result;
-
-            return View(Articles);
-        }
-
-        // GET: Articles/Details/2
         public ActionResult Details(int id)
         {
-            DetailsArticle ViewModel = new DetailsArticle();
-
-            //objective: retrieve an article from API
-            //curl https://localhost:44338/api/articledata/findarticle/{id}
-
-            string url = "articledata/findarticle/" + id;
+            string url = "api/author/" + id;
             HttpResponseMessage response = client.GetAsync(url).Result;
 
+            Author author = response.Content.ReadAsAsync<Author>().Result;
 
-            ArticlesDto SelectedArticle = response.Content.ReadAsAsync<ArticlesDto>().Result;
+            return View(author);
+        }
 
-            ViewModel.SelectedArticle = SelectedArticle;
+        public ActionResult New()
+        {
+            return View();
+        }
 
-            return View(ViewModel);
+        [HttpPost]
+        public ActionResult Create(Author author)
+        {
+            string url = "api/author";
+            HttpResponseMessage response = client.PostAsJsonAsync(url, author).Result;
 
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            string url = "api/author/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            Author selectedAuthor = response.Content.ReadAsAsync<Author>().Result;
+
+            return View(selectedAuthor);
+        }
+
+        public ActionResult Update(Author author)
+        {
+            string url = "api/author/" + author.AuthorID;
+            HttpResponseMessage response = client.PutAsJsonAsync(url, author).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            string url = "api/author/" + id;
+            HttpResponseMessage response = client.DeleteAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         public ActionResult Error()
         {
             return View();
         }
-
-        // GET: Article/New
-        [Authorize]
-        public ActionResult New()
-        {
-            //GET: api/author
-
-            string url = "author/";
-            HttpResponseMessage response = client.GetAsync(url).Result;
-            IEnumerable<AuthorsDto> AuthorsOptions = response.Content.ReadAsAsync<IEnumerable<AuthorsDto>>().Result;
-
-            return View(AuthorsOptions);
-        }
-
-
-        //Post: Article/Create
-        [HttpPost]
-        [Authorize]
-        public ActionResult Create(Article Article)
-        {
-            GetApplicationCookie();
-            //new Article using the API
-            //curl -H "Content-Type:application/json" -d @Article.json https://localhost:44338/api/Articledata/addArticle 
-            string url = "articledata/addarticle";
-
-            string jsonpayload = jss.Serialize(Article);
-            //Debug.WriteLine(jsonpayload);
-
-            HttpContent content = new StringContent(jsonpayload);
-            content.Headers.ContentType.MediaType = "application/json";
-
-            HttpResponseMessage response = client.PostAsync(url, content).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                return RedirectToAction("List");
-            }
-            else
-            {
-                return RedirectToAction("Error");
-            }
-        }
-
-        // GET: Article/Edit/2
-        [Authorize]
-        public ActionResult Edit(int id)
-        {
-            UpdateArticle ViewModel = new UpdateArticle();
-
-            //existing Article information
-            string url = "Articledata/findarticle/" + id;
-            HttpResponseMessage response = client.GetAsync(url).Result;
-            ArticlesDto SelectedArticle = response.Content.ReadAsAsync<ArticlesDto>().Result;
-            ViewModel.SelectedArticle = SelectedArticle;
-
-            // all authors to choose from when updating this article
-            url = "author/";
-            response = client.GetAsync(url).Result;
-            IEnumerable<AuthorsDto> AuthorsOptions = response.Content.ReadAsAsync<IEnumerable<AuthorsDto>>().Result;
-
-            ViewModel.AuthorsOptions = AuthorsOptions;
-
-            return View(ViewModel);
-        }
-
-        // POST: Article/Update/2
-        [HttpPost]
-        [Authorize]
-        public ActionResult Update(int id, Article Article)
-        {
-            GetApplicationCookie();
-            string url = "articledata/updatearticle/" + id;
-            string jsonpayload = jss.Serialize(Article);
-            HttpContent content = new StringContent(jsonpayload);
-            content.Headers.ContentType.MediaType = "application/json";
-            HttpResponseMessage response = client.PostAsync(url, content).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                return RedirectToAction("List");
-            }
-            else
-            {
-                return RedirectToAction("Error");
-            }
-        }
-
-        // GET: Article/Delete/2
-        [Authorize]
-        public ActionResult DeleteConfirm(int id)
-        {
-            string url = "articledata/findarticle/" + id;
-            HttpResponseMessage response = client.GetAsync(url).Result;
-            ArticlesDto SelectedArticles = response.Content.ReadAsAsync<ArticlesDto>().Result;
-            return View(SelectedArticles);
-        }
-
-        // POST: Article/Delete/2
-        [HttpPost]
-        [Authorize]
-        public ActionResult Delete(int id)
-        {
-            GetApplicationCookie();
-            string url = "articledata/deletearticle/" + id;
-            HttpContent content = new StringContent("");
-            content.Headers.ContentType.MediaType = "application/json";
-            HttpResponseMessage response = client.PostAsync(url, content).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                return RedirectToAction("List");
-            }
-            else
-            {
-                return RedirectToAction("Error");
-            }
-        }
     }
 }
-
